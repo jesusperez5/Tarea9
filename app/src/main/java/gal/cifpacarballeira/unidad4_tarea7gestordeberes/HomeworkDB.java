def5d77ccdbc60dbd4a1e418da89f1b6935ece3c
jesusperez5 <jesuspepr554@gmail.com>
2025-01-19 20:06:14 +0100

    creando

--- app/src/main/java/gal/cifpacarballeira/unidad4_tarea7gestordeberes/HomeworkDB.java
+++ app/src/main/java/gal/cifpacarballeira/unidad4_tarea7gestordeberes/HomeworkDB.java
@@ -1,2 +1,35 @@
-package gal.cifpacarballeira.unidad4_tarea7gestordeberes;public class HomeworkDB {
+package gal.cifpacarballeira.unidad4_tarea7gestordeberes;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class HomeworkDB extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "homeworksdb";
+    private static final int DB_VERSION = 4;
+    private static final String TABLE_HOMEWORKS = "homeworks";
+
+    public HomeworkDB(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_HOMEWORKS + " (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "subject TEXT, " +
+                "description TEXT, " +
+                "due_date TEXT, " +
+                "is_completed INTEGER)";
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP table if exists homeworks");
+    }
 }
--- app/src/main/java/gal/cifpacarballeira/unidad4_tarea7gestordeberes/HomeworkRepository.java
+++ app/src/main/java/gal/cifpacarballeira/unidad4_tarea7gestordeberes/HomeworkRepository.java
@@ -1,2 +1,64 @@
-package gal.cifpacarballeira.unidad4_tarea7gestordeberes;public class HomeworkRepository {
+package gal.cifpacarballeira.unidad4_tarea7gestordeberes;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeworkRepository {
+
+    private final HomeworkDB homeworkDB;
+
+    public HomeworkRepository(Context context) {
+        homeworkDB = new HomeworkDB(context);
+    }
+
+    public long insertHomework(Homework homework) {
+        SQLiteDatabase db = homeworkDB.getWritableDatabase();
+        ContentValues values = homework.toContentValues();
+        long result = db.insert("homeworks", null, homework.toContentValues());
+        homework.setId(result);
+        db.close();
+        return result;
+    }
+
+    public List<Homework> getAllHomework() {
+        SQLiteDatabase db = homeworkDB.getReadableDatabase();
+        List<Homework> homeworkList = new ArrayList<>();
+        Cursor cursor = db.query("homeworks", null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                homeworkList.add(Homework.fromCursor(cursor));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return homeworkList;
+    }
+
+    public int updateHomework(long id, Homework homework) {
+        SQLiteDatabase db = homeworkDB.getWritableDatabase();
+        int rowsUpdated = db.update(
+               "homeworks",
+                homework.toContentValues(),
+                "id" + " = ?",
+                new String[]{String.valueOf(id)}
+        );
+        db.close();
+        return rowsUpdated;
+    }
+
+    public int deleteHomework(long id) {
+        SQLiteDatabase db = homeworkDB.getWritableDatabase();
+        int rowsDeleted = db.delete(
+                "homeworks",
+                "id" + " = ?",
+                new String[]{String.valueOf(id)}
+        );
+        db.close();
+        return rowsDeleted;
+    }
 }
